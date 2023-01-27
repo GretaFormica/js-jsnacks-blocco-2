b309b319adfc8642ced1e091a7eff709ca98592f
GretaFormica <greta.formica@gmail.com>
2023-01-27 17:08:57 +0100

    SNACK 3 : format

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,10 +1,10 @@
 //array
-const numeri = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
+const numeri = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 let somma = 0;
 
-for(i = 0; i < numeri.length; i++) {
+for (i = 0; i < numeri.length; i++) {
 
-    if(i % 2 !=0){
+    if (i % 2 != 0) {
         somma += numeri[i];
     }
 }
